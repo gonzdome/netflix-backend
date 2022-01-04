96c9c8c64a8a40d3e996822bf3bd21a594d4bb49
Gabriel Gonzalez Domeniconi <gabrielgonzalezdomeniconi@gmail.com>
2022-01-04 15:58:05 -0300

    Created endpoints for: list one, list all, create, update, delete register

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ routes.use(morgan('dev'));
 // set the morgan dependency to only work in dev-mode
 
 routes.get('/', (req, res) => {
-  res.json();
+  res.json({ mensagem: 'Mostrar todos os registros' });
 });
 // endpoint to get all registrations
 
@@ -16,7 +16,23 @@ routes.get('/:id', (req, res) => {
   const { id } = req.params;
   res.json({ mensagem: `Mostrar apenas o registro de ID: '${id}'` });
 });
-// endpoint to get a single registration using the ID as a parameter
+// endpoint to get a single register using the ID as a parameter
 
+routes.post('/', (req, res) => {
+  const { body } = req;
+  res.json(body);
+});
+// endpoint to create new register
+
+routes.put('/:id', (req, res) => {
+  const id = req.params;
+  res.json({ mensagem: `Atualizar somente o registo de ID: ${id}` });
+});
+// endpoint to update a register
+
+routes.delete('/:id', (req, res) => {
+  const id = req.params;
+  res.json({ mensagem: `Deletar somente o registo de ID: ${id}` });
+});
 module.exports = routes;
 // export routes
